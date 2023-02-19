a44bafdf95e4bfe4d43eba6b4aaa962d782333c9
”DmimaN” <karim.rnd@yandex.ru>
2023-02-20 00:28:19 +0300

    add swing GUI start version not work 2

--- src/CaesarGUI.java
+++ src/CaesarGUI.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CaesarGUI extends JFrame implements ActionListener {
+public class CaesarGUI extends JFrame{
     JRadioButton Encode;
     JRadioButton Decode;
     JRadioButton BrutForce;
@@ -12,19 +12,26 @@ public class CaesarGUI extends JFrame implements ActionListener {
     CaesarGUI(){
         jFrame = new JFrame("Caesar  Cipher");
         jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        //jFrame.setSize(400,800);
         jFrame.setLocationRelativeTo(null);
         jFrame.setResizable(true);
         jFrame.setLayout(new GridLayout(0, 1, 0, 4));
         JButton start = new JButton("Start");
         panel2 = new JPanel(new GridLayout(0, 1, 0, 5));
         panel2.add(start);
+        JLabel lable = new JLabel("Input") ;
+        JFileChooser fileChooser = new JFileChooser("/");
+        JPanel panel3 = new JPanel(new GridLayout(0, 2, 0, 1));
+        panel3.add(lable);
+        panel3.add(fileChooser);
+
 
         ButtonGroup buttonGroup = new ButtonGroup();
         JPanel panel1 = new JPanel(new GridLayout(0, 3, 0, 1));
-        Encode = new JRadioButton("Encode");
+        Encode = new JRadioButton("Encode", true);
         Decode = new JRadioButton("Decode");
         BrutForce = new JRadioButton("BrutForce");
+
+
 
 
         panel1.add(Encode);
@@ -34,31 +41,41 @@ public class CaesarGUI extends JFrame implements ActionListener {
         buttonGroup.add(Encode);
         buttonGroup.add(Decode);
         buttonGroup.add(BrutForce);
+       ActionListener radioBatt = new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               if(Encode.isSelected()){
+                   jFrame.add(panel2);
+               }else if (Decode.isSelected()){
+                   jFrame.add(panel2);
+               } else if (BrutForce.isSelected()) {
+                   jFrame.remove(panel2);
+               }
+               jFrame.pack();
+               jFrame.setVisible(true);
+           }
+       };
+        Encode.addActionListener(radioBatt);
+        Decode.addActionListener(radioBatt);
+        BrutForce.addActionListener(radioBatt);
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (start.isSelected());
+                System.out.println("ssss");
+            }
+        });
 
-        Encode.addActionListener(this);
-        Decode.addActionListener(this);
-        BrutForce.addActionListener(this);
 
         jFrame.add(panel1);
+        if(Encode.isSelected()){
+            jFrame.add(panel2);
+        }
+        jFrame.add(panel3);
         jFrame.pack();
         jFrame.setVisible(true);
 
-
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==Encode){
-            jFrame.add(panel2);
-            jFrame.pack();
-            jFrame.setVisible(true);
-            System.out.println("jjjj");
-        }else {
-            jFrame.remove(panel2);
-            jFrame.pack();
-            jFrame.setVisible(true);
-        }
 
-
-    }
 }
